22339268b9c3f7146460db501d5c0bbdd6446584
Jim Santora <jim.santora@epicgames.com>
2024-11-25 00:00:39 -0500

    Exporting fix for newer next.js versions.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/resume',
-};
+  basePath: '/resume', // Replace 'your-repo-name' with your actual repository name
+  assetPrefix: '/resume/', // Add this line, replace with your repository name
+}
 
 export default nextConfig;
